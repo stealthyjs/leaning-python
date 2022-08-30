30c03cba25cf3e3cece4e6513d8343c0574d86a5
stealthyjs <61940923+stealthyjs@users.noreply.github.com>
2022-08-30 12:51:27 -0400

    Add files via upload

---
+++ comments-and-print.py
@@ -0,0 +1,11 @@
+#! /usr/bin/python3
+
+"""
+This is a multiline comment. Ugly.
+"""
+
+# This is a single line comment. MUCH BETTER :)
+
+name="John Snow"
+
+print("Greetings to " + name + ". Now you know more than nothing.")
